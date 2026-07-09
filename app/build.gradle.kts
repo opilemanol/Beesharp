b7c9bf01c7f777c89d2207349217db2c17be9d8e
opilemanol <70860412+opilemanol@users.noreply.github.com>
2026-07-09 19:21:54 +0100

    Update build.gradle.kts

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -22,7 +22,7 @@ testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 signingConfigs {
     create("release") {
         // Look for the environment variable, default to local root directory if empty
-        val keystorePath = System.getenv("KEYSTORE_PATH") ?: "${rootDir}/my-upload-key.jks"
+        val keystorePath = System.getenv("KEYSTORE_PATH") ?: "${rootDir}/my-upload-keybeesharp"
         storeFile = file(keystorePath)
         
         // This handles both GitHub environment variables and local properties gracefully
