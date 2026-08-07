287acae5e11e06e9e4b0ee40b4accd91b9fa2821
opilemanol <70860412+opilemanol@users.noreply.github.com>
2026-08-07 14:29:23 +0100

    Update versionCode and versionName to 5.0

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -14,8 +14,8 @@ defaultConfig {
     applicationId = "com.aistudio.beesharp.wbyqkz"
    minSdk = 24
 targetSdk = 36
-    versionCode = 4
-    versionName = "4.0"
+    versionCode = 5
+    versionName = "5.0"
 testInstrumentationRunner = "androidx.test.runner.AndroidJUnitRunner"
 }
 
